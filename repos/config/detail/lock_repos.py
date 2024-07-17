f3b522b91ff99db43dbf79c64ca650c2eaf436eb
Evan Flynn <evan.flynn@apex.ai>
2024-07-17 07:09:02 -0700

    Add workspace name to update command hint
    Signed-off-by: Evan Flynn <evan.flynn@apex.ai>

--- repos/config/detail/lock_repos.py
+++ repos/config/detail/lock_repos.py
@@ -53,7 +53,7 @@ def main():
 
     with open(args.lock, "w", encoding='utf8') as lock_file:
         print(
-            "#\n#   To update, call `bazel run //repos/config:repos_lock.update` with the right distro set in the WORKSPACE\n#",
+            "#\n#   To update, call `bazel run @rules_ros//repos/config:repos_lock.update` with the right distro set in the WORKSPACE\n#",
             file = lock_file
         )
         yaml.dump(repos, lock_file, default_flow_style=False, allow_unicode=True)
